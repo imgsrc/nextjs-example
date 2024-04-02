8c718881c60ac932850885439eba5c93befa4a8d
Igor Nikolaev <i.nikolaev@docrobot.ru>
2024-04-02 18:31:49 +0200

    add

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -12,11 +12,11 @@ export default function handler(
   const { query } = req.query;
   searchBlogPages(query as string).then((resp) => {
     const result = resp?.map(
-      ({ fields: { title, description, slug } }: any) => ({
+      ({ fields: { title, slug } }: any) => ({
         title,
         href: `/blog/${slug}`,
       }),
     );
-    res.status(200).json(result);
+    res.status(200).json(result as any);
   });
 }
